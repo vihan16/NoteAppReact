1e11a26b5bc3bed42924df12053057f0b4088f39
Vihan Patidar <123254447+vihan16@users.noreply.github.com>
2024-09-13 10:58:23 +0000

    some change

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,12 +7,15 @@ function Card(){
         <div className=' relative w-60 h-72 p-5 rounded-[40px] bg-zinc-800 text-white overflow-hidden'>
             <FaRegFileAlt/>
             <p className='text-sm leading-tight mt-5 '>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas possimus enim placeat fuga.</p>
-            <div className='footer absolute bottom-0 py-3 px-6  w-full h-16 left-0'>
-                <div className='flex items-center justify-between'>
+            <div className='footer absolute bottom-0   w-full  left-0'>
+                <div className='flex items-center py-3 px-6  mb-2 justify-between'>
                     <h5>.4mb</h5>
-                    <span className='w-6 h-6 bg-yellow-200 rounded-full flex items-center justify-centre'>
+                    <span className='w-6 h-6 bg-yellow-200 rounded-full flex items-center justify-center'>
                         <GoDownload size='1em' color='#000'/>
                     </span>
+                </div>
+                <div className='tag w-full py-4 bg-yellow-200 flex justify-center item-center'>
+                    <h3 className='text-md text-black'>Download Now</h3>
                 </div>
             </div>
         </div>
--- src/components/ForGround.jsx
+++ src/components/ForGround.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import Card from './Card'
 
 function ForGround() {
+  const data = [
+    {desc: "Lorem ipsim jn dnsjn sajkcnj snci nsjnck js", filesize:".9mb",clos}
+  ]
   return (
     <div className='fixed top-0 left-0 z-[3] w-full h-full '>
      <Card/>
